f876d5c967706cc0d9418427a871e891607cca47
GitHub Action <action@github.com>
2024-11-01 06:59:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/zzzxb/annication/MyBundle.kt
+++ src/main/kotlin/com/github/zzzxb/annication/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.zzzxb.annication
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/zzzxb/annication/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/zzzxb/annication/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.zzzxb.annication.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
